c40baad4e8109c5b81492c346a0eaff91c2e70c4
Rui Braz Correia Junior <ruibraz@pop-os.localdomain>
2021-03-02 21:43:56 -0300

    Adicionando migrations com flyway no banco h2.

--- src/main/kotlin/br/com/braz/rui/Application.kt
+++ src/main/kotlin/br/com/braz/rui/Application.kt
@@ -1,7 +1,10 @@
 package br.com.braz.rui
 
 import io.micronaut.runtime.Micronaut.*
+import org.h2.tools.Server
+
 fun main(args: Array<String>) {
+	Server.createWebServer().start()
 	build()
 	    .args(*args)
 		.packages("br.com.braz.rui")
---
+++ src/main/resources/db/migration/V1__create_table_task.sql
@@ -0,0 +1,8 @@
+create table T_TAREFA (
+    ID int not null primary key,
+    DATA_INICIO varchar(100) not null,
+    DESCRICAO varchar(100) not null,
+    NOME varchar(100) not null,
+    STATUS int not null,
+    TEMPO varchar(100) not null
+);
